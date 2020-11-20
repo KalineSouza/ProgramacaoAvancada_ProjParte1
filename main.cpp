ea036b520239783ffcd169218e12d410676fe16d
Kaline Souza dos Santos <67524329+KalineSouza@users.noreply.github.com>
2020-11-19 22:30:55 -0300

    Add files via upload
    Programa Principal

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "sculptor.h"
+#include "sculptor.cpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    Sculptor Desenho(100,100,100);
+    Desenho.setColor(1,0.7,0.8,0.1);
+    Desenho.putVoxel(0,0,0);
+    Desenho.cutVoxel(0,0,0);
+    Desenho.putBox(0,0,0,0,0,0);
+    Desenho.cutBox(0,0,0,0,0,0);
+    Desenho.setColor(1,0,0,1);
+   // Desenho.putSphere(0,0,0,0);
+   // Desenho.cutSphere(0,0,0,0);
+    Desenho.putEllipsoid(50,60,50,40,49,30);
+    Desenho.cutEllipsoid(47,50,20,27,42,40);
+    Desenho.writeOFF("write.off");
+    return 0;
+}
